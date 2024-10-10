0b42410b773b1bd661157e347f820a853c336f55
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-10-10 14:40:00 -0400

    Dash Effect Fix

--- project-cypher/Scripts/Player/Dash.gd
+++ project-cypher/Scripts/Player/Dash.gd
@@ -14,17 +14,25 @@ var normalSpeed
 func _physics_process(delta):
 	if Input.is_action_pressed("dash") and player.isMoving and canDash:
 		boost()
+	shadowCloneBehavior()
 
 # Starts timer duration for dash, it creates clone effect, and it boost the character speed
 func boost():
 	dashDuration.start()
 	hurtbox.is_invicible = true
-	dashClones.emitting = true
-	sprite.modulate.a = 0.7
+	sprite.modulate.a = 0.5
 	normalSpeed = player.playerSpeed
 	boostSpeed = player.playerSpeed * 2
 	player.playerSpeed = boostSpeed
 	canDash = false
+	dashClones.emitting = true
+
+# Checks if the player is moving or not while the boost is active
+func shadowCloneBehavior():
+	if !player.isMoving and !dashDuration.is_stopped():
+		dashClones.emitting = false
+	if player.isMoving and !dashDuration.is_stopped():
+		dashClones.emitting = true
 
 # Once the dash is over it starts a cooldown timer
 func _on_dash_duration_timeout() -> void:
