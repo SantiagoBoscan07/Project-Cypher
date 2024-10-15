57fc39133ab4795b3b2ed2fabd6ba8845b060081
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-10-14 21:04:48 -0400

    Menu and Key Mapping

---
+++ project-cypher/Scripts/Menu/InputSettings.gd
@@ -0,0 +1,79 @@
+extends Control
+
+@export var gameOptions: Control
+@export var actionList: VBoxContainer
+@onready var inputButtonScene = preload("res://Scenes/Menu/inputButton.tscn")
+
+var isRemapping = false
+var actionToRemap = null
+var remappingButton = null
+
+var inputActions: Dictionary = {
+	"moveUp": "Move Up",
+	"moveLeft": "Move Left",
+	"moveDown": "Move Down",
+	"moveRight": "Move Right",
+	"shoot": "Shoot",
+	"dash": "Dash",
+	"shuffle": "Shuffle",
+	"decipher": "Decipher",
+	"moveSlotLeft": "Move Slot Left",
+	"moveSlotRight": "Move Slot Right",
+	"pause": "Pause"
+}
+
+func _ready():
+	createActionList()
+
+func createActionList():
+	var button
+	var actionLabel
+	var inputLabel
+	var events
+	InputMap.load_from_project_settings()
+	for item in actionList.get_children():
+		item.queue_free()
+	for action in inputActions:
+		button = inputButtonScene.instantiate()
+		actionLabel = button.find_child("LabelAction")
+		inputLabel = button.find_child("LabelInput")
+		actionLabel.text = inputActions[action]
+		events = InputMap.action_get_events(action)
+		if events.size()  > 0:
+			inputLabel.text = events[0].as_text().trim_suffix(" (Physical)")
+		else:
+			inputLabel.text = ""
+		actionList.add_child(button)
+		button.pressed.connect(_on_input_pressed.bind(button, action))
+
+func _on_input_pressed(button, action):
+	if !isRemapping:
+		isRemapping = true
+		actionToRemap = action
+		remappingButton = button
+		button.find_child("LabelInput").text = "Press Key To Bind..."
+
+func _input(event):
+	if isRemapping:
+		if (
+			event is InputEventKey # Here I can add Controller later
+		):
+			InputMap.action_erase_events(actionToRemap)
+			InputMap.action_add_event(actionToRemap, event)
+			updateActionList(remappingButton, event)
+			isRemapping = false
+			actionToRemap = null
+			remappingButton = null
+
+func updateActionList(button, event):
+	button.find_child("LabelInput").text = event.as_text().trim_suffix(" (Physical)")
+
+
+
+func _on_default_button_pressed():
+	createActionList()
+
+
+func _on_back_menu_pressed() -> void:
+	gameOptions.visible = true
+	visible = false
--- project-cypher/Scripts/Menu/Menu.gd
+++ project-cypher/Scripts/Menu/Menu.gd
@@ -1,41 +1,15 @@
 extends Control
 
-@export var initial: VBoxContainer
-@export var options: VBoxContainer
-@export var controllers: VBoxContainer
+@export var inputSettings: Control
 
-# Makes the sure that the initial buttons are shown
 func _ready():
-	initial.visible = true
+	visible = true
 
-# When the play button is pressed, the game starts
-func _on_play_pressed():
+
+func _on_play_game_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Test/test.tscn") # this is going to change
 
-# When the quit button is pressed, the game closes
-func _on_exit_pressed():
-	get_tree().quit()
 
-# Shows the option menu
 func _on_options_pressed():
-	initial.visible = false
-	options.visible = true
-
-# Shows the controller menu
-func _on_controller_pressed():
-	options.visible = false
-	controllers.visible = true
-
-# Shows the volume menu
-func _on_volume_pressed():
-	pass # Replace 
-
-# Goes back from options menu to main menu
-func _on_back_pressed():
-	initial.visible = true
-	options.visible = false
-
-# Goes from controller menu to options menu
-func _on_back_controller_pressed() -> void:
-	controllers.visible = false
-	options.visible = true
+	inputSettings.visible = true
+	visible = false
