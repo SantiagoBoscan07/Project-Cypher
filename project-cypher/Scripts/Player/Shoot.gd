54c32640996e0169473a9ad49933381b4a937829
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-10-21 19:24:43 -0400

    Clone auto shoot

--- project-cypher/Scripts/Player/Shoot.gd
+++ project-cypher/Scripts/Player/Shoot.gd
@@ -5,17 +5,25 @@ class_name Shoot
 @export var player: CharacterBody2D
 @export var shootingTimer: Timer
 @onready var projectilePreload: PackedScene = preload("res://Scenes/Player/playerProjectile.tscn")
+@export var isClone: bool = false
+@export var cloneTimer: Timer
 var canShoot: bool = true
 var projectile
 
+func _ready():
+	if isClone:
+		if cloneTimer:
+			cloneTimer.start()
+
 # If shoot button is pressed it calls the shoot function
-func _physics_process(delta):
-	if Input.is_action_pressed("shoot") and canShoot:
+func  _input(event):
+	if Input.is_action_pressed("shoot") and canShoot and !isClone:
 		shoot()
 
 # Loads the projectile scene and creates an instantiate in the scene
 func shoot():
-	shootingTimer.start()
+	if !isClone:
+		shootingTimer.start()
 	projectile = projectilePreload.instantiate()
 	projectile.position = shootingPosition.global_position
 	if player.lastDirectionFacing == Vector2.ZERO:
@@ -28,3 +36,7 @@ func shoot():
 # Once the timer runs out, the player can shoot again
 func _on_shooting_timer_timeout() -> void:
 	canShoot = true
+
+
+func _on_clone_timer_timeout() -> void:
+	shoot()
