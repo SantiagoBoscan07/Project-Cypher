7208a08d940f1751ed9db6136dfce2ad7be40bfe
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-10-22 17:24:18 -0400

    hotfix

--- project-cypher/Scripts/Player/ProjectileSpawner.gd
+++ project-cypher/Scripts/Player/ProjectileSpawner.gd
@@ -1,10 +1,39 @@
-extends Area2D
+extends Node2D
 
-var bullethell
+class_name ProjectileSpawner
+
+@export var bullethell: PackedScene
+@export_range(0,2*PI) var alpha: float = 0.0
+@export var bulletspawner: Marker2D
+@export var power_timer: Timer
+@export var durationTimer: Timer
+@export var body: CharacterBody2D
+var canStorm: bool = true
+var theta: float = 0.0
 var speed = 100
 var direction = Vector2.RIGHT
 
+func _ready():
+	Signals.connect("startStorm", timer)
 
+func get_vector(angle):
+	theta = angle + alpha
+	return Vector2(cos(theta), sin(theta))
 
-func _physics_process(delta):
-	position += direction * speed * delta
+func start_storm(angle):
+	var bullethell = bullethell.instantiate()
+	bullethell.position = bulletspawner.global_position
+	bullethell.z_index = body.z_index - 1
+	bullethell.direction = get_vector(angle)
+	get_tree().current_scene.call_deferred("add_child", bullethell)
+
+func _on_bullet_storm_duration_timeout():
+	power_timer.stop()
+
+func timer():
+	power_timer.start()
+	durationTimer.start()
+	start_storm(theta)
+
+func _on_bullet_storm_cooldown_timeout():
+	start_storm(theta)
