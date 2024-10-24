b38ede4e96fbbad8e53fc2382e0894486de894bf
Leo <leonardo.reyes0040@gmail.com>
2024-10-24 16:42:12 -0400

    Tank Enemy
    Leonardo
    Leo
    Denise

--- project-cypher/Scripts/Enemy/MoveState.gd
+++ project-cypher/Scripts/Enemy/MoveState.gd
@@ -1,13 +1,22 @@
 extends State
-
+@export var player: CharacterBody2D
+@export var desiredSpeed: Vector2
+@export var maxSpeedChange: float=20.0
+@export var enemy: CharacterBody2D
+var playerposition
 # what the enemy is going to do when they enter the state
 func enter():
 	print("Here something will happen when the state is loaded")
-
+func update(_delta):
+	if player:
+		playerposition=player.position
 # what is happening with the enemy every frame
 func physicsUpdate(_delta):
+	var speed: float = 20.0
+	enemy.velocity.x = move_toward(player.velocity.x, desiredSpeed.x, maxSpeedChange)
+	enemy.velocity.y = move_toward(player.velocity.y, desiredSpeed.y, maxSpeedChange)
+	enemy.move_and_slide()
 	# update the position of the enemy so that it moves towards the player
-	pass
 
 # what the enemy is going to do when they exit the state
 func exit():
