d127d3d1776bb3807e1f85f4262edc956d26f26c
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-10-28 23:05:17 -0400

    tank enemy

--- project-cypher/Scripts/Enemy/MoveState.gd
+++ project-cypher/Scripts/Enemy/MoveState.gd
@@ -1,21 +1,22 @@
 extends State
-@export var player: CharacterBody2D
-@export var desiredSpeed: Vector2
-@export var maxSpeedChange: float=20.0
+
+@export var moveSpeed: float=5.0
 @export var enemy: CharacterBody2D
-var playerposition
+@export var moveTimer: Timer
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("Player")
+var playerPos
+var targetPos
+
 # what the enemy is going to do when they enter the state
 func enter():
-	print("Here something will happen when the state is loaded")
-func update(_delta):
-	if player:
-		playerposition=player.position
+	moveTimer.start()
 # what is happening with the enemy every frame
 func physicsUpdate(_delta):
-	var speed: float = 20.0
-	enemy.velocity.x = move_toward(player.velocity.x, desiredSpeed.x, maxSpeedChange)
-	enemy.velocity.y = move_toward(player.velocity.y, desiredSpeed.y, maxSpeedChange)
-	enemy.move_and_slide()
+	if player:
+		playerPos = player.position
+		targetPos = (playerPos - enemy.position).normalized()
+		if enemy.position.distance_to(playerPos) > 3:
+			enemy.position += targetPos * moveSpeed * _delta
 	# update the position of the enemy so that it moves towards the player
 
 # what the enemy is going to do when they exit the state
@@ -25,4 +26,4 @@ func exit():
 
 # whenever the move timer runsout, you're transitioning to the shoot state
 func _on_move_timer_timeout():
-	pass # Replace with function body.
+	stateTransition.emit(get_parent().currentState, "ShootState")
