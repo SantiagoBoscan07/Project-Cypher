c6acd6a61d09cecc8752dd529e26dfd9533d82f1
crimino450 <crmolina24@gmail.com>
2024-10-31 16:42:30 -0400

    Changeable array prototype for player to set numbers for mechanic
    Cristian, Eddie

--- project-cypher/Scripts/ImplementationCode.gd
+++ project-cypher/Scripts/ImplementationCode.gd
@@ -1,15 +1,15 @@
 extends Node
 
-var test = 0:
-	set(value):
-		test = value
-	get:
-		return test
+var cypherarray = [1, 2, 3]
 
+func setCypherArray(index, value):
+	cypherarray[index] = value
+func getCypherArray(index):
+	return cypherarray[index]
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
