a422e2739fa36369efbfd3400ef319cbabcf88e2
crimino450 <crmolina24@gmail.com>
2024-11-03 19:34:34 -0500

    Made Array Script Global
    The script that contains the array that the player manipulates through the UI is now global, and its variables and function can be accessed from any other script, and added functions to go increment and decrement with array position is being accessed, with wraparound (decreasing from array 0 goes to the last array position)

--- project-cypher/Scripts/ImplementationCode.gd
+++ project-cypher/Scripts/ImplementationCode.gd
@@ -1,16 +1,22 @@
 extends Node
 
-var cypherarray = [1, 2, 3]
+var playerCypherArray = [1, 2, 3]
+var playerArrayIndex = 0
 
-func setCypherArray(index, value):
-	cypherarray[index] = value
-func getCypherArray(index):
-	return cypherarray[index]
+func setPlayerCypherArray(value):
+	playerCypherArray[playerArrayIndex] = value
+func getPlayerCypherArray():
+	return playerCypherArray[playerArrayIndex]
 	
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func playerRight():
+	if (playerArrayIndex < playerCypherArray.size() - 1):
+		playerArrayIndex += 1
+	else	:
+		playerArrayIndex = 0
+	
+	print(playerArrayIndex)
+func playerLeft():
+	if (playerArrayIndex > 0):
+		playerArrayIndex -= 1
+	else:
+		playerArrayIndex = playerCypherArray.size() - 1
