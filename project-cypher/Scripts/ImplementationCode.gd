5907dd6c47238a94cda64243ffdcb6f01231c738
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-11-04 15:28:25 -0500

    fixes

--- project-cypher/Scripts/ImplementationCode.gd
+++ project-cypher/Scripts/ImplementationCode.gd
@@ -13,10 +13,18 @@ func playerRight():
 		playerArrayIndex += 1
 	else	:
 		playerArrayIndex = 0
-	
-	print(playerArrayIndex)
+	#print(playerArrayIndex)
+
 func playerLeft():
 	if (playerArrayIndex > 0):
 		playerArrayIndex -= 1
 	else:
 		playerArrayIndex = playerCypherArray.size() - 1
+	#print(playerArrayIndex)
+
+func _unhandled_input(event: InputEvent):
+	# Add check to verify the state of the game and move index accordingly
+	if Input.is_action_just_pressed("moveLeft"):
+		playerLeft()
+	if Input.is_action_just_pressed("moveRight"):
+		playerRight()
