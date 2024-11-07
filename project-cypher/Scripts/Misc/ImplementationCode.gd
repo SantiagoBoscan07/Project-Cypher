3deb528a3c7d8f10a8674b86d4324e3511f957ca
Vestroia292 <ksun008@fiu.edu>
2024-11-07 16:30:54 -0500

    CypherShuffle implementation complete
    Created method to change the player's cypher and edit the value
    Chris
    Chris
    Eddie
    Kevin

--- project-cypher/Scripts/Misc/ImplementationCode.gd
+++ project-cypher/Scripts/Misc/ImplementationCode.gd
@@ -1,10 +1,22 @@
 extends Node
 
-var playerCypherArray = [1, 2, 3]
+var playerCypherArray = [1, 1, 1] #FIXME Need logic to reset cyphers to default (1, 1, 1) at beginning of scene
 var playerArrayIndex = 0
+@export var cypherRange = 3
+var tempVal = 0
 
 func setPlayerCypherArray(value):
-	playerCypherArray[playerArrayIndex] = value
+	tempVal = playerCypherArray[playerArrayIndex] + value
+	
+	if (tempVal > cypherRange):
+		playerCypherArray[playerArrayIndex] = 1
+	elif (tempVal < 1):
+		playerCypherArray[playerArrayIndex] = cypherRange
+	else:
+		playerCypherArray[playerArrayIndex] = tempVal
+	print(playerCypherArray)
+
+	
 func getPlayerCypherArray():
 	return playerCypherArray[playerArrayIndex]
 	
@@ -22,9 +34,30 @@ func playerLeft():
 		playerArrayIndex = playerCypherArray.size() - 1
 	#print(playerArrayIndex)
 
+func playerUp():
+	setPlayerCypherArray(1)
+	#if (playerCypherArray[playerArrayIndex] != cypherRange):
+		#playerCypherArray[playerArrayIndex] += 1
+	#else	:
+		#playerCypherArray[playerArrayIndex] = 1
+	#print(playerArrayIndex)
+
+func playerDown():
+	setPlayerCypherArray(-1)
+	#if (playerCypherArray[playerArrayIndex] != 1):
+		#playerCypherArray[playerArrayIndex] -= 1
+	#else:
+		#playerCypherArray[playerArrayIndex] = cypherRange
+	#print(playerArrayIndex)
+
+
 func _unhandled_input(event: InputEvent):
 	# Add check to verify the state of the game and move index accordingly
 	if Input.is_action_just_pressed("moveLeft"):
 		playerLeft()
 	if Input.is_action_just_pressed("moveRight"):
 		playerRight()
+	if Input.is_action_just_pressed("moveUp"):
+		playerUp()
+	if Input.is_action_just_pressed("moveDown"):
+		playerDown()
