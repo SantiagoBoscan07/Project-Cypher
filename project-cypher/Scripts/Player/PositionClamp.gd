a11237c4c6edbbd382eeab1598f636a3ac8cc9f2
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-11-29 21:49:49 -0500

    clamp fix

--- project-cypher/Scripts/Player/PositionClamp.gd
+++ project-cypher/Scripts/Player/PositionClamp.gd
@@ -2,12 +2,12 @@ extends Node
 class_name PositionClamp
 
 @export var body: CharacterBody2D
-@export var margin = 24
+@export var margin = 12
 
-var left_border = -24
+var left_border = 0
 var right_border = 480
 var bottom_border = 208
-var top_border = -24
+var top_border = 0
 
 # Clamp the player within a small space
 func _process(delta):
