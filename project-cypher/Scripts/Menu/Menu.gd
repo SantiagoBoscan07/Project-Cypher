13f04c89d9771131d294b4960ad7e4763e526217
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2024-12-05 14:53:50 -0500

    level2 bug

--- project-cypher/Scripts/Menu/Menu.gd
+++ project-cypher/Scripts/Menu/Menu.gd
@@ -60,7 +60,7 @@ func _on_level_2_pressed():
 	Signals.connect("endFadeBlack", startLevel2)
 
 func startLevel2():
-	get_tree().change_scene_to_file("res://Scenes/Levels/Level2.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/level2.tscn")
 
 
 func _on_level_3_pressed() -> void:
